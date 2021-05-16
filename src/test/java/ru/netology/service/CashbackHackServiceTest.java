d6bbdc3dd640972fc7e9e7fd27b4b2e497e2a260
Maria Sedova <mariassedova@mail.ru>
2021-05-16 19:02:40 +0300

    testNG + service tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    public CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnBonusIfAmountUnder1000() {
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnBonusIfAmountOver1000() {
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnBonusIfAmount1000() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+}
